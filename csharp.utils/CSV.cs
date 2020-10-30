5ccebec3e1d33996d75ec440428e2f8b3bc181f8
omvf <61068260+omvf@users.noreply.github.com>
2020-10-30 21:07:41 +0000

    feat: hello world + csv reader

---
+++ csharp.utils/CSV.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp.utils
+{
+    public class CSV
+    {
+        public static String[] ReadCSV(string filename, char separator = ',')
+        {
+            return File.ReadAllText(filename).Split(separator);
+        }
+    }
+}
