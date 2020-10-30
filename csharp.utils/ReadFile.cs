15ef08aaa59315dc7ae79d3241de392d407864b5
omvf <61068260+omvf@users.noreply.github.com>
2020-10-30 21:28:21 +0000

    feat: read file

---
+++ csharp.utils/ReadFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp.utils
+{
+    public class ReadFile
+    {
+        public static string ReadFile(string filename)
+        {
+            return File.ReadAllText(filename);
+        }
+    }
+}
