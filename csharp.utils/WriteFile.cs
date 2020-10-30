472426f2713240aee1419b49734928e0c096445a
omvf <61068260+omvf@users.noreply.github.com>
2020-10-30 21:42:45 +0000

    feat:write-file

---
+++ csharp.utils/WriteFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp.utils
+{
+    public class WriteFile
+    {
+        public static void WriteFile(string content)
+        {
+            File.WriteAllLines(content);
+        }
+    }
+}
